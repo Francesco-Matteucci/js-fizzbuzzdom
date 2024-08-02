f9b3c86d7fe079d6f68d54786742eda0f22005cc
Francesco-Matteucci <francescomatteucci@gmail.com>
2024-08-02 14:23:37 +0200

    Milestone 2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 
-/* MILESTONE 2:
-Creiamo un elemento che faccia da contenitore nel DOM e poi riempiamolo con i degli elementi via JS.
-Possiamo usare varie tecniche  (template literals, innerHTML, appendecc)
-MILESTONE 3:
+
+/*MILESTONE 3:
 Applichiamo stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
-Come abbiamo visto ci sono usare varie tecniche (style , className, classList)
+Come abbiamo visto ci sono usare varie tecniche(style, className, classList)
 Se siete a corto di idee per lo stile, potreste prendere spunto dallo screenshot fornito in consegna. */
 
 
@@ -24,18 +22,32 @@ Se siete a corto di idee per lo stile, potreste prendere spunto dallo screenshot
 const result = document.getElementById('result');
 console.log(result);
 
-
+// creo un elemento ul
+const list = document.createElement('ul');
 
 //* Utilizzo un for per stampare dei numeri da 1 a 100, utilizzando Fizz per i multipli di 3, Buzz per i multipli di 5, e FizzBuzz per i multipli di entrambi
 for (let i = 1; i <= 100; i++) {
+    //creo un elemento li
+    const listItems = document.createElement('li');
+    let content;
+
+    //determino il contenuto dei li
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
+        content = 'FizzBuzz';
     } else if (i % 3 === 0) {
-        console.log('Fizz');
+        content = 'Fizz';
     } else if (i % 5 === 0) {
-        console.log('Buzz');
+        content = 'Buzz';
     } else {
-        console.log(i);
+        content = i;
     }
+
+    //imposto il contenuto dei li
+    listItems.textContent = content;
+
+    //Aggiungo i li alla lista ul
+    list.appendChild(listItems);
 }
 
+//Aggiungo la lista al div del DOM
+result.appendChild(list);
