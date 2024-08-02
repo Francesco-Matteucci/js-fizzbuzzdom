75e326143b3c7be62587c821bf1cc4e36e3caf7a
Francesco-Matteucci <francescomatteucci@gmail.com>
2024-08-02 15:35:53 +0200

    adding styles

--- js/script.js
+++ js/script.js
@@ -25,25 +25,37 @@ console.log(result);
 // creo un elemento ul
 const list = document.createElement('ul');
 
+//aggiungo una classe alla lista
+list.classList.add('row', 'row-cols-2', 'row-cols-sm-3', 'row-cols-md-4', 'row-cols-lg-6');
+
 //* Utilizzo un for per stampare dei numeri da 1 a 100, utilizzando Fizz per i multipli di 3, Buzz per i multipli di 5, e FizzBuzz per i multipli di entrambi
 for (let i = 1; i <= 100; i++) {
     //creo un elemento li
     const listItems = document.createElement('li');
+
     let content;
+    let className;
 
     //determino il contenuto dei li
     if (i % 3 === 0 && i % 5 === 0) {
         content = 'FizzBuzz';
+        className = 'fizz-buzz'
     } else if (i % 3 === 0) {
         content = 'Fizz';
+        className = 'fizz'
     } else if (i % 5 === 0) {
         content = 'Buzz';
+        className = 'buzz'
     } else {
         content = i;
+        className = 'normal-items'
     }
 
     //imposto il contenuto dei li
     listItems.textContent = content;
+    listItems.className = className;
+
+    listItems.classList.add('col', 'g-4', 'd-flex', 'justify-content-center')
 
     //Aggiungo i li alla lista ul
     list.appendChild(listItems);
